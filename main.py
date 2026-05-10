bbef2603444fb8991cb1858de69b779094523431
annashestakova <annashestakova2011@gmail.com>
2026-05-10 19:13:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ INSTAGRAM = "https://www.instagram.com/volos_capsula/"
 PHONE = os.environ.get("MASTER_PHONE","+375291234567")
 CARD = os.environ.get("CARD_NUMBER","0000 0000 0000 0000")
 
-GUIDE_URL = "https://github.com/annashestakova/capsula-bot/raw/main/%D0%A1%D0%B5%D0%BA%D1%80%D0%B5%D1%82%D1%8B_%D0%B4%D0%BE%D0%BB%D0%B3%D0%BE%D0%B9_%D0%BD%D0%BE%D1%81%D0%BA%D0%B8_%D0%BD%D0%B0%D1%80%D0%B0%D1%89%D0%B8%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F.pdf"
+GUIDE_URL = "https://raw.githubusercontent.com/annashestakova/capsula-bot/main/%D0%A1%D0%B5%D0%BA%D1%80%D0%B5%D1%82%D1%8B_%D0%B4%D0%BE%D0%BB%D0%B3%D0%BE%D0%B9_%D0%BD%D0%BE%D1%81%D0%BA%D0%B8_%D0%BD%D0%B0%D1%80%D0%B0%D1%89%D0%B8%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F.pdf"
 
 SERVICE_DURATION = {
     "Снятие капсул":       2,
